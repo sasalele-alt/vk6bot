39d0ddcb5a17bfd44281737b2b288343ef24a814
sasalele-alt <81077451+sasalele-alt@users.noreply.github.com>
2021-03-21 15:55:28 +0300

    Update vk_bot_group.py

--- vk_bot_group.py
+++ vk_bot_group.py
@@ -281,13 +281,12 @@ class Group_bot():
 
             if event.text.capitalize() == 'Расписание' and self.hw_start == True:
                 print('1')
-                http_proxy  = "http://10.10.1.10:3128"
-                https_proxy = "https://10.10.1.11:1080"
-                ftp_proxy   = "ftp://10.10.1.10:3128"
-                headers = 'User-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36 OPR/74.0.3911.218 (Edition Yx 08)'
-                proxyDict = {'http'  : http_proxy, 'https' : https_proxy, 'ftp'   : ftp_proxy}
-
-                r = requests.get('http://p11505.edu35.ru/gmraspisanie/izmeneniya', headers=headers, proxies=proxyDict)
+                header = { 
+'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36', 
+'upgrade-insecure-requests': '1', 
+'cookie': 'mos_id=CllGxlx+PS20pAxcIuDnAgA=; session-cookie=158b36ec3ea4f5484054ad1fd21407333c874ef0fa4f0c8e34387efd5464a1e9500e2277b0367d71a273e5b46fa0869a; NSC_WBS-QUBG-jo-nptsv-WT-443=ffffffff0951e23245525d5f4f58455e445a4a423660; rheftjdd=rheftjddVal; _ym_uid=1552395093355938562; _ym_d=1552395093; _ym_isad=2' 
+}
+                r = requests.get('http://p11505.edu35.ru/gmraspisanie/izmeneniya', headers=header)
                 print(r.status_code)
                 self.__timetable(event)
                 self.default_setting()
